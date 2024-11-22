0de4754fbb20c00be50076f3cebecf29a741be68
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2024-11-22 23:25:40 +0800

    .

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -16,6 +16,7 @@ const ProductCard = ({
    imgSrc: string;
    setProducts: any;
 }) => {
+   const [img, setImg] = useState(imgSrc);
    const [deleting, setDeleting] = useState(false);
    const handleDelete = async () => {
       setDeleting(true);
@@ -33,30 +34,28 @@ const ProductCard = ({
       }
    };
    return (
-      <div className="bg-secondary h-auto overflow-hidden rounded-lg shadow-lg transition-transform duration-500 hover:-translate-y-1">
+      <div className="h-auto overflow-hidden rounded-lg bg-secondary shadow-lg transition-transform duration-500 hover:-translate-y-1">
          <div className="relative h-40 w-full">
             <Image
-               src={
-                  imgSrc ||
-                  "https://images.unsplash.com/photo-1471014645228-037cfcd2bdc1?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-               }
+               src={img}
+               onError={() => setImg("/backupimg.avif")}
                alt={name}
                layout="fill"
                objectFit="cover"
                priority={true}
             />
          </div>
-         <div className="text-textclr flex flex-col gap-1 p-3 font-bold">
+         <div className="flex flex-col gap-1 p-3 font-bold text-textclr">
             <p>{name}</p>
             <p>${price}</p>
             <div className="text-black">
-               <button className="bg-edit mr-2 cursor-pointer rounded-sm p-1 active:scale-95">
+               <button className="mr-2 cursor-pointer rounded-sm bg-edit p-1 active:scale-95">
                   <BiEdit className="size-6" />
                </button>
                <button
                   onClick={handleDelete}
                   disabled={deleting}
-                  className={`${deleting && "brightness-75"} bg-delete rounded-sm p-1 transition-all active:scale-95`}
+                  className={`${deleting && "brightness-75"} rounded-sm bg-delete p-1 transition-all active:scale-95`}
                >
                   <BiTrash className="size-6" />
                </button>
